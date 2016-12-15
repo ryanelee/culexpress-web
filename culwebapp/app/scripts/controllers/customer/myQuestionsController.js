1b50887c71307a2859e86f455408e50884f66591
En Li <eleeryan@gmail.com>
2016-12-14 17:10:00 -0800

    Fixed bug: loading warehouse in ask question page

--- culwebapp/app/scripts/controllers/customer/myQuestionsController.js
+++ culwebapp/app/scripts/controllers/customer/myQuestionsController.js
@@ -28,8 +28,8 @@ angular
                         model.messageType = result.data[0].typeID;
                     });
                 orderSvr.getWarehouses().then(function (result) {
-                    $scope.questionWarehouses = [result.data[2]];//TODO这里暂时只获取了OR仓库
-                    $scope.model.receivedWarehouseNumber = result.data[2].warehouseNumber;
+                    $scope.questionWarehouses = result.data;
+                    $scope.model.receivedWarehouseNumber = result.data[0].warehouseNumber;
                 });
             }
 
