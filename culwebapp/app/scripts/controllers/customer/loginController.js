e6ac922b88698c2a3db96255369ac99a57d3f7b8
TimLiu1 <995332120@qq.com>
2017-01-11 23:36:16 +0800

    bug修复和增加新功能

--- culwebapp/app/scripts/controllers/customer/loginController.js
+++ culwebapp/app/scripts/controllers/customer/loginController.js
@@ -2,56 +2,64 @@
 
 angular
     .module('culwebApp')
-    .controller('LoginController', ['$rootScope', '$scope', 'AuthService', '$state',
-    function ($rootScope, $scope, AuthService, $state) {
-        $scope.rememberMe = false;
-        $scope.loginError = undefined;
-        $scope.showLoginError = false;
+    .controller('LoginController', ['$rootScope', '$scope', 'AuthService', '$state', '$window',
+        function ($rootScope, $scope, AuthService, $state, $window) {
+            $scope.rememberMe = false;
+            $scope.loginError = undefined;
+            $scope.showLoginError = false;
+            console.log(1)
+            // $window.localStorage.removeItem('user')
+            $scope.user = angular.fromJson($window.localStorage.getItem('user'))
+            $scope.emailAddress = $scope.user.emailAddress
+            if($scope.user.rememberMe){
+            $scope.password = $scope.user.password
+            }
+            $scope.login = function () {
+                if ($('.state-error').length > 0) return;
+                var loginData = {
+                    emailAddress: $scope.emailAddress,
+                    password: $scope.password,
+                    rememberMe: $scope.rememberMe
+                };
 
-        $scope.login = function () {
-            if ($('.state-error').length > 0) return;
-            var loginData = {
-                emailAddress: $scope.emailAddress,
-                password: $scope.password,
-                rememberMe: $scope.rememberMe
-            };
+                $scope.$root.isLogined = false;
+                AuthService.login(loginData)
+                    .then(function (result) {
+                        if (result.data && result.data.photoUrl === null) {
 
-            $scope.$root.isLogined = false;
-            AuthService.login(loginData)
-                .then(function (result) {
-                    if (result.data && result.data.photoUrl === null) {
-                        if (result.data.gender === 'M')
-                            result.data.photoUrl = '/assets/img/culwebapp/customer/profile/no-photo-male.jpg';
-                        else
-                            result.data.photoUrl = '/assets/img/culwebapp/customer/profile/no-photo-female.jpg';
-                    }
-                    AuthService.clearStorage();
-                    AuthService.addStorage(angular.extend(result.data, { password: $scope.password }), loginData.rememberMe);
-
-                    if (result.headers('Token')) {
-                        if (loginData.rememberMe) {
-                            localStorage.setItem('cul-token', result.headers('Token'));
+                            if (result.data.gender === 'M')
+                                result.data.photoUrl = '/assets/img/culwebapp/customer/profile/no-photo-male.jpg';
+                            else
+                                result.data.photoUrl = '/assets/img/culwebapp/customer/profile/no-photo-female.jpg';
                         }
-                        else {
+                        AuthService.clearStorage();
+                        AuthService.addStorage(angular.extend(result.data, { password: $scope.password }), loginData.rememberMe);
+
+                        if (result.headers('Token')) {
+                            if (loginData.rememberMe) {
+                                $window.localStorage.setItem('user', angular.toJson(loginData));
+                                localStorage.setItem('cul-token', result.headers('Token'));
+                            }
+                            // else {
                             sessionStorage.setItem('cul-token', result.headers('Token'));
+                            // }
                         }
-                    }
 
-                    $scope.$root.currentUser = result.data;
+                        $scope.$root.currentUser = result.data;
 
-                    $rootScope.isLackProfile = !result.data.firstName || !result.data.lastName;
-                    $rootScope.isLogined = true;
+                        $rootScope.isLackProfile = !result.data.firstName || !result.data.lastName;
+                        $rootScope.isLogined = true;
 
 
-                    if ($scope.$root.isLackProfile) {
-                        $state.go('customer.myaccount', { anchorid: 'profile' });
-                    } else {
-                        $state.go('customer.myhome');
-                    }
-                },
-                function (result) {
-                    $scope.showLoginError = true;
-                    $scope.loginError = result.data.message;
-                });
-        };
-    }]);
+                        if ($scope.$root.isLackProfile) {
+                            $state.go('customer.myaccount', { anchorid: 'profile' });
+                        } else {
+                            $state.go('customer.myhome');
+                        }
+                    },
+                    function (result) {
+                        $scope.showLoginError = true;
+                        $scope.loginError = result.data.message;
+                    });
+            };
+        }]);
