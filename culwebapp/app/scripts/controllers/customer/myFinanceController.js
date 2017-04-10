3f6cc038b33c5d092b6d15d78061975e3946085f
timliu <tim@MacxdeMacBook-Pro.local>
2017-04-10 23:18:25 +0800

    ok

--- culwebapp/app/scripts/controllers/customer/myFinanceController.js
+++ culwebapp/app/scripts/controllers/customer/myFinanceController.js
@@ -3,90 +3,95 @@
 angular
     .module('culwebApp')
     .controller('MyFinanceController', ['$scope', '$stateParams', '$window', '$location', 'Customer',
-    function ($scope, $stateParams, $window, $location, customer) {
-        if (App) {
-            App.init();
-            App.initCounter();
-            App.initScrollBar();
-        }
-
-        $scope.currentTabId = $stateParams.tabId || 'recharge';
-
-        $scope.currentUser = $scope.$root.currentUser;
-
-
-        //$scope.$root.autologin(function (result) {
-        //    $scope.currentUser.accountBalance = result.accountBalance;
-        //    return false;
-        //});
-
-        customer.getCustomerInfo($scope.currentUser.customerNumber)
-        .then(function (result) {
-            $scope.currentUser.accountBalance = result.data.accountBalance;
-        });
-
-
-        var model = $scope.model = {
-
-        }, operationType = 1;
-
-        if ($scope.currentTabId === 'debit') {
-            operationType = 2;
-        }
-
-        $scope.userPay = function () {
-            var customerNumber = $scope.$root.currentUser.customerNumber;
-            if (!customerNumber) {
-                alertify.alert('提醒', '请先登录.', 'warning'); return false;
+        function($scope, $stateParams, $window, $location, customer) {
+            if (App) {
+                App.init();
+                App.initCounter();
+                App.initScrollBar();
             }
-            if (!model.payAmount) {
-                alertify.alert('提醒', '请输入充值金额.', 'warning'); return false;
-            }
-            if (model.payAmount < 0.1) {
-                alertify.alert('提醒', '充值金额不能小于0.1', 'warning'); return false;
-            }
-            window.open('rechargepage.html?ra=' + encodeURIComponent(model.payAmount) + '&cn=' + encodeURIComponent(customerNumber));
-        }
+
+            $scope.currentTabId = $stateParams.tabId || 'recharge';
+
+            $scope.currentUser = $scope.$root.currentUser;
 
 
-        $scope.myfinanceListData = [];
-        $scope.pagedDebitOptions = $scope.pagedOptions = {
-            total: 0,
-            size: 10
-        }
-        var loadFinanceLog = function (index, callback) {
-            customer.getFinanceLog(index, $scope.currentUser.customerNumber, operationType)
-                .then(function (result) {
-                    callback && callback(result);
+            //$scope.$root.autologin(function (result) {
+            //    $scope.currentUser.accountBalance = result.accountBalance;
+            //    return false;
+            //});
+
+            customer.getCustomerInfo($scope.currentUser.customerNumber)
+                .then(function(result) {
+                    $scope.currentUser.accountBalance = result.data.accountBalance;
                 });
-        }
 
-        $scope.onPaged = function (pageIndex) {
-            loadFinanceLog(pageIndex, function () {
-                $scope.myfinanceListData = result.data.data;
-                $scope.pagedOptions.total = result.data.pageInfo.totalCount;
-            });
-        }
 
-        $scope.myDebitListData = []
+            var model = $scope.model = {
 
-        $scope.onDebitPaged = function (pageIndex) {
-            loadFinanceLog(pageIndex, function () {
-                $scope.myDebitListData = result.data.data;
-                $scope.pagedDebitOptions.total = result.data.pageInfo.totalCount;
-            });
-        }
-        if ($location.path() === '/customer/myfinancedetail/recharge' || $location.path() === '/customer/myfinancedetail/debit') {
-            loadFinanceLog(1, function (result) {
-                if (operationType === 1) {
+                },
+                operationType = 1;
+
+            if ($scope.currentTabId === 'debit') {
+                operationType = 2;
+            }
+
+            $scope.userPay = function() {
+                var customerNumber = $scope.$root.currentUser.customerNumber;
+                if (!customerNumber) {
+                    alertify.alert('提醒', '请先登录.', 'warning');
+                    return false;
+                }
+                if (!model.payAmount) {
+                    alertify.alert('提醒', '请输入充值金额.', 'warning');
+                    return false;
+                }
+                if (model.payAmount < 0.1) {
+                    alertify.alert('提醒', '充值金额不能小于0.1', 'warning');
+                    return false;
+                }
+                window.open('rechargepage.html?ra=' + encodeURIComponent(model.payAmount) + '&cn=' + encodeURIComponent(customerNumber));
+            }
+
+
+            $scope.myfinanceListData = [];
+            $scope.pagedDebitOptions = $scope.pagedOptions = {
+                total: 0,
+                size: 10
+            }
+            var loadFinanceLog = function(index, callback) {
+                customer.getFinanceLog(index, $scope.currentUser.customerNumber, operationType)
+                    .then(function(result) {
+                        console.log(result);
+                        callback && callback(result);
+                    });
+            }
+
+            $scope.onPaged = function(pageIndex) {
+                loadFinanceLog(pageIndex, function() {
                     $scope.myfinanceListData = result.data.data;
                     $scope.pagedOptions.total = result.data.pageInfo.totalCount;
-                }
-                else if (operationType === 2) {
+                });
+            }
+
+            $scope.myDebitListData = []
+
+            $scope.onDebitPaged = function(pageIndex) {
+                loadFinanceLog(pageIndex, function(result) {
                     $scope.myDebitListData = result.data.data;
                     $scope.pagedDebitOptions.total = result.data.pageInfo.totalCount;
-                }
-            });
-        }
+                });
+            }
+            if ($location.path() === '/customer/myfinancedetail/recharge' || $location.path() === '/customer/myfinancedetail/debit') {
+                loadFinanceLog(1, function(result) {
+                    if (operationType === 1) {
+                        $scope.myfinanceListData = result.data.data;
+                        $scope.pagedOptions.total = result.data.pageInfo.totalCount;
+                    } else if (operationType === 2) {
+                        $scope.myDebitListData = result.data.data;
+                        $scope.pagedDebitOptions.total = result.data.pageInfo.totalCount;
+                    }
+                });
+            }
 
-    }]);
+        }
+    ]);
