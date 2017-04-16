efea97f430077747bd69501bdcb9854cdb8ddef4
timliu <tim@MacxdeMacBook-Pro.local>
2017-04-16 12:51:23 +0800

    sort

--- culadminapp/app/scripts/directives/print-order.js
+++ culadminapp/app/scripts/directives/print-order.js
@@ -7,75 +7,119 @@
  * # printOrder
  */
 angular.module('culAdminApp')
-  .directive('printOrder', ["$timeout", "orderService", function ($timeout, orderService) {
-      return {
-          templateUrl: "views/templates/common/print-order_tpl.html",
-          restrict: 'E',
-          replace: true,
-          scope: true,
-          link: function postLink($scope, $element, attrs) {
-              var barCodeSettings = {
-                  addQuietZone: "1",
-                  barHeight: "25",
-                  barWidth: "1",
-                  bgColor: "#FFFFFF",
-                  color: "#000000",
-                  moduleSize: "5",
-                  output: "css",
-                  posX: "10",
-                  posY: "20"
-              }
+    .directive('printOrder', ["$timeout", "orderService", function($timeout, orderService) {
+        return {
+            templateUrl: "views/templates/common/print-order_tpl.html",
+            restrict: 'E',
+            replace: true,
+            scope: true,
+            link: function postLink($scope, $element, attrs) {
+                var barCodeSettings = {
+                    addQuietZone: "1",
+                    barHeight: "25",
+                    barWidth: "1",
+                    bgColor: "#FFFFFF",
+                    color: "#000000",
+                    moduleSize: "5",
+                    output: "css",
+                    posX: "10",
+                    posY: "20"
+                }
 
-              $scope.$on("print-order.action", function (e, orderNumbers) {
-                  if (!angular.isArray(orderNumbers)) $scope.orderNumbers = [orderNumbers];
-                  else $scope.orderNumbers = orderNumbers;
-                  $scope.printDate = new Date();
-                  $scope.dataList = [];
-                  $scope.packagesList = [];
-                  var _options = {
-                      pageInfo: {
-                          pageSize: 99999,
-                          pageIndex: 1
-                      },
-                      orderNumber: orderNumbers
-                  }
-                  orderService.getList(_options, function (result) {
-                    //   console.log("result>"+JSON.stringify(result));
-                      $scope.dataList = result.data;
-                    //console.log(result.data);
-                      $.each($scope.dataList, function (i, _data) {
-                          _data._shippingFeeTotal = 0;
-                          $.each(_data.outboundPackages, function (i, outboundPackage) {
-                              _data._shippingFeeTotal += outboundPackage.shippingFee;
-                          });
-                          _data._shippingFeeTotal = _data._shippingFeeTotal.toFixed(2);
-                      });
+                $scope.$on("print-order.action", function(e, orderNumbers) {
+                    if (!angular.isArray(orderNumbers)) $scope.orderNumbers = [orderNumbers];
+                    else $scope.orderNumbers = orderNumbers;
+                    $scope.printDate = new Date();
+                    $scope.dataList = [];
+                    $scope.packagesList = [];
+                    var _options = {
+                        pageInfo: {
+                            pageSize: 99999,
+                            pageIndex: 1
+                        },
+                        orderNumber: orderNumbers
+                    }
+                    orderService.getList(_options, function(result) {
+                        console.log("美好的世界1")
+                            //   console.log("result>"+JSON.stringify(result));
+                        $scope.dataList = result.data;
+                        //console.log(result.data);
+                        $.each($scope.dataList, function(i, _data) {
+                            _data._shippingFeeTotal = 0;
+                            $.each(_data.outboundPackages, function(i, outboundPackage) {
+                                _data._shippingFeeTotal += outboundPackage.shippingFee;
+                            });
+                            _data._shippingFeeTotal = _data._shippingFeeTotal.toFixed(2);
+                        });
 
-                      _render();
-                  });
-                  //$.each($scope.orderNumbers, function (index, ordeNumber) {
-                  //    orderService.getDetail(ordeNumber, function (result) {
-                  //        $scope.dataList.push(result);
-                  //        _render();
-                  //    });
-                  //});
-              });
 
-              var _render = function () {
-                  if ($scope.orderNumbers.length == $scope.dataList.length) {
-                      $timeout(function () {
-                          $.each($element.find("div[orderNumber]"), function (index, el) {
-                              $(el).barcode($(el).attr("orderNumber"), "code128", barCodeSettings);
-                          });
+                        var shelf = { flag: "shelf", data: [] }
+                        let j = 0;
+                        for (var i = 0; i < $scope.dataList.length; i++) {
+                            console.log("美好的世界")
+                            j++;
+                            $scope.dataList[i].inboundPackages.forEach(function(e) {
+                                let detail = {
+                                    customerNumber: $scope.dataList[i].customerNumber,
+                                    orderNumber: $scope.dataList[i].orderNumber,
+                                    cartonCount: $scope.dataList[i].cartonCount,
+                                    payDate: $scope.dataList[i].payDate,
+                                    tip: $scope.dataList[i].tip,
+                                }
+                                if (e.shelfNumber) {
+                                    detail.sort = e.shelfNumber.substr(0, 1);
+                                } else {
+                                    detail.sort = 'H'
+                                }
+                                detail.trackingNumber = e.trackingNumber;
+                                detail.shelfNumber = e.shelfNumber;
+                                detail.packageWeight = e.packageWeight;
+                                shelf.data.push(detail);
+                            })
 
-                          $.each($element.find("div[trackingNumber]"), function (index, el) {
-                              $(el).barcode($(el).attr("trackingNumber"), "code128", barCodeSettings);
-                          });
+                            if (j % 10 == 0 || i == $scope.dataList.length - 1) {
+                                var tempshelf = _.sortBy(shelf.data, function(item) {
+                                    return item.sort;
+                                });
+                                shelf.data = tempshelf;
+                                $scope.dataList.splice(i + 1, 0, shelf);
+                                shelf = { flag: "shelf", data: [] }
+                                i++;
 
-                          $element.children().jqprint();
-                      }, 500);
-                  }
-              }
-          }
-      };
-  }]);
+                            }
+                        }
+                        console.log($scope.dataList);
+                        _render();
+                    });
+
+
+
+
+
+
+                    //$.each($scope.orderNumbers, function (index, ordeNumber) {
+                    //    orderService.getDetail(ordeNumber, function (result) {
+                    //        $scope.dataList.push(result);
+                    //        _render();
+                    //    });
+                    //});
+                });
+
+                var _render = function() {
+                    if ($scope.dataList.length == $scope.dataList.length) {
+                        $timeout(function() {
+                            $.each($element.find("div[orderNumber]"), function(index, el) {
+                                $(el).barcode($(el).attr("orderNumber"), "code128", barCodeSettings);
+                            });
+
+                            $.each($element.find("div[trackingNumber]"), function(index, el) {
+                                $(el).barcode($(el).attr("trackingNumber"), "code128", barCodeSettings);
+                            });
+
+                            $element.children().jqprint();
+                        }, 500);
+                    }
+                }
+            }
+        };
+    }]);
