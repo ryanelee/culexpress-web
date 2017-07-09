c5f7673286bc1e17d729f143a20836b7e71a2118
Lisa <youer@youerdeMacBook-Pro.local>
2017-07-09 23:09:12 +0800

    bug fixed

--- culadminapp/app/scripts/controllers/customer/customerdetail.js
+++ culadminapp/app/scripts/controllers/customer/customerdetail.js
@@ -23,6 +23,7 @@ angular.module('culAdminApp')
           customerService.getDetail($scope.customerNumber, function (result) {
               $scope.data = result;
               console.log($scope.data)
+              _buildUpload($('#uploadImg'), "images");
           });
 
           $scope.btnSave = function () {
@@ -86,4 +87,43 @@ angular.module('culAdminApp')
                   }
               });
           }
+
+          $scope.btnMessagePush = function () {
+            if (!$scope.message.message) {
+                plugMessenger.info("请填写留言信息");
+                return;
+            }
+
+            $scope.message.messageType = 29;
+            $scope.message.images = $scope.data.images;
+            console.log($scope.message);
+            customerService.addCustomerMessage($scope.message).then(function (result) {
+                //$scope.refreshMessage();
+                $scope.message.message = "";
+                $scope.data.images = "";
+                $("#uploadImg_show").attr('src',''); 
+            })
+          }
+
+          //----------upload file START----------
+            var _buildUpload = function ($el, key) {
+                var _$panel = $el.parents(".fileupload-buttonbar:first");
+                $el.fileupload({
+                    url: cul.apiPath + '/files/upload',
+                    type: "post",
+                    headers: {
+                        token: sessionStorage.getItem("token")
+                    }
+                }).bind('fileuploadprogress', function (e, result) {
+                    var progress = parseInt(result.loaded / result.total * 100, 10);
+                    _$panel.find("#progress").css('width', progress + '%');
+                }).bind('fileuploaddone', function (e, data) {
+                    _$panel.find("#file_btn_text").text("重新上传");
+                    $scope.$apply(function () {
+                        $scope.data[key] = data.result.url;
+                        console.log( $scope.data[key]);
+                    });
+                });
+            }
+            //----------upload file END----------
       }]);
