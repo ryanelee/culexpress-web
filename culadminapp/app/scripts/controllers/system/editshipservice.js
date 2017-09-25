faa54888e239af48aa725e55f45d774e952a9e5d
Lisa <Lisa@LisaMacBook-Pro.local>
2017-09-25 22:29:33 +0800

    新需求

---
+++ culadminapp/app/scripts/controllers/system/editshipservice.js
@@ -0,0 +1,76 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name culAdminApp.controller:ShipserviceEditCtrl
+ * @description
+ * # ShipserviceEditCtrl
+ * Controller of the culAdminApp
+ */
+angular.module('culAdminApp')
+    .controller('ShipserviceEditCtrl', ['$scope', '$location', '$window', 'plugMessenger','channelService',
+        function($scope, $location, $window, plugMessenger, channelService) {
+            this.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+            $scope.form = {};
+
+            $scope.form = $location.search().item;
+
+            if ($scope.form) {
+                // $scope.flag = '1'
+                $scope.flag = $location.search().flag;
+                console.log($scope.flag)
+            } else {
+                $scope.form = {
+                    status: '1'
+                }
+            }
+
+            // 返回列表
+            $scope.back = function() {
+                $location.path('/system/channelist').search({});
+            }
+
+            /**
+             * 保存渠道数据
+             * @return {[type]}      [description]
+             */
+            $scope.saveChannel= function() {
+                    if (!$scope.form.channelName) {
+                        plugMessenger.info("请输入渠道名称!");
+                        return;
+                    }
+                    if (!$scope.form.clearAddress) {
+                        plugMessenger.info("请输入清关地址!");
+                        return;
+                    }
+                    if (!$scope.form.channelDesc) {
+                        plugMessenger.info("请输入渠道描述!");
+                        return;
+                    }
+                    channelService.createChannel($scope.form, function(res) {
+                        if (res.code == '000') {
+                            plugMessenger.success("创建成功");
+                            $location.path('/system/channelist')
+                        } else {
+                            plugMessenger.success(" 创建失败：" + req.msg);
+                        }
+                    })
+                }
+                //更新仓库
+            $scope.update = function() {
+                channelService.updateChannel($scope.form, function(res) {
+                    if (res.code == '000') {
+                        plugMessenger.success("更新成功");
+                        $location.path('/system/channelist')
+                    } else {
+                        plugMessenger.success("更新失败" + req.msg);
+                    }
+                })
+            }
+
+        }
+    ]);
---
+++ culadminapp/app/scripts/controllers/system/shipservicelist.js
@@ -0,0 +1,103 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name culAdminApp.controller:ShipserviceCtrl
+ * @description
+ * # ShipserviceCtrl
+ * Controller of the culAdminApp
+ */
+angular.module('culAdminApp')
+    .controller('ShipserviceCtrl', ['$scope', '$location', '$window', 'sysroleService', 'customerService', 'channelService', 'plugMessenger',"storage",
+        function($scope, $location, $window, sysroleService, customerService, channelService, plugMessenger,storage) {
+            this.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+
+            $scope.dataList = [];
+            /*search bar*/
+            $scope.searchBar = {
+                status: "",
+                keywordType: "channelName"
+            }
+            $scope.pagination = {
+                pageSize: "20",
+                pageIndex: 1,
+                totalCount: 0
+            }
+            $scope.tempSearchBar = angular.copy(storage.getSearchObject());
+            if ($scope.tempSearchBar) {
+                $scope.searchBar = $scope.tempSearchBar ? $scope.tempSearchBar : $scope.searchBar;
+            }
+            //  storage.session.setObject("searchBar", $scope.searchBar);
+
+            var _filterOptions = function() {
+                var _options = {
+                    "pageInfo": $scope.pagination
+                }
+                if (!!$scope.searchBar.status) {
+                    _options["status"] = $scope.searchBar.status;
+                }
+                if (!!$scope.searchBar.keywords) {
+                    _options[$scope.searchBar.keywordType] = $scope.searchBar.keywords;
+                }
+                return angular.copy(_options);
+            }
+
+            $scope.getData = function() {
+                storage.session.setObject("searchBar", $scope.searchBar);
+                channelService.getChannelList(_filterOptions(), function(result) {
+                    $scope.dataList = result.data.data;
+                    $scope.pagination.totalCount = result.data.pageInfo.totalCount;
+                });
+            }
+            $scope.btnSearch = function() {
+                $scope.dataList = [];
+                $scope.pagination.pageIndex = 1;
+                $scope.pagination.totalCount = 0;
+                $scope.getData();
+            }
+            $scope.getData();
+
+            // 新建服务跳转
+            $scope.addShipservice = function() {
+                $location.path('/system/editshipservice').search({});
+            }
+            // 修改渠道
+            $scope.edit = function(item) {
+                if (!!item) $location.search({ item: item, flag: "edit" });
+                $location.path('/system/editchannel')
+            }
+
+            $scope.detail  = function(item) {
+                if (!!item) $location.search({ item: item, flag: "detail" });
+                $location.path('/system/editchannel')
+            }
+
+            $scope.delete = function(item) {
+                plugMessenger.confirm("确认删除该渠道吗?", function(isOk) {
+                    if (isOk) {
+                        channelService.deleteChannel({ channelId: item.channelId }, function(res) {
+                            if (res.code == '000') {
+                                plugMessenger.success("删除成功");
+                                $scope.getData();
+                            } else {
+                                plugMessenger.error("删除失败");
+
+                            }
+                        })
+                    }
+                });
+            }
+
+            // 返回列表
+            $scope.back = function() {
+                //$location.path('/system/rolelist');
+                $window.sessionStorage.setItem("historyFlag", 1);                 
+                $window.history.back();
+            }
+        }
+    ]);
+
