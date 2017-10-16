22f3ac9edd329ea7061037da4cacb529660b088f
Lisa <Lisa@LisaMacBook-Pro.local>
2017-10-16 23:15:27 +0800

    完善

--- culadminapp/app/scripts/controllers/system/editshipservice.js
+++ culadminapp/app/scripts/controllers/system/editshipservice.js
@@ -60,88 +60,103 @@ angular.module('culAdminApp')
              * @return {[type]}      [description]
              */
             $scope.saveShipservice= function() {
-                _shipserviceFilter();
-                shipService.createShipservice($scope.form, function(res) {
-                    if (res.code == '000') {
-                        plugMessenger.success("创建成功");
-                        $location.path('/system/shipservicelist')
-                    } else {
-                        plugMessenger.success(" 创建失败：" + req.msg);
-                    }
-                })
+                if (_shipserviceFilter() === false) {
+                    console.log('false')
+                    return;
+                } else {
+                    console.log('true')
+                    $scope.getFormWarehouseList();
+                    $scope.getFormChannelList();
+                    $scope.getFormCarrierList();
+                    console.log("$scope.form")
+                    console.log($scope.form)
+                    shipService.createShipservice($scope.form, function(res) {
+                        if (res.code == '000') {
+                            plugMessenger.success("创建成功");
+                            $location.path('/system/shipservicelist')
+                        } else {
+                            plugMessenger.success(" 创建失败：" + req.msg);
+                        }
+                    })
+                }
             }
 
             //更新转运服务
             $scope.update = function() {
-                _shipserviceFilter();
-                getFormWarehouseList();
-                getFormChannelList();
-                getFormCarrierList();
-                shipService.updateShipservice($scope.form, function(res) {
-                    if (res.code == '000') {
-                        plugMessenger.success("更新成功");
-                        $location.path('/system/shipservicelist')
-                    } else {
-                        plugMessenger.success("更新失败" + req.msg);
-                    }
-                })
+                if (_shipserviceFilter() === false) {
+                    return;
+                } else {
+                    $scope.getFormWarehouseList();
+                    $scope.getFormChannelList();
+                    $scope.getFormCarrierList();
+                    console.log("$scope.form")
+                    console.log($scope.form)
+                    shipService.updateShipservice($scope.form, function(res) {
+                        if (res.code == '000') {
+                            plugMessenger.success("更新成功");
+                            $location.path('/system/shipservicelist')
+                        } else {
+                            plugMessenger.success("更新失败" + req.msg);
+                        }
+                    })
+                }
             }
 
             // 新增和更新数据选项
             var _shipserviceFilter = function () {
                 if (!$scope.form.shipServiceName) {
                     plugMessenger.info("请输入服务名称!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.serviceSummary) {
                     plugMessenger.info("请输入服务简介!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.firstWeightRMB) {
                     plugMessenger.info("请输入普通用户人名币首重费用!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.continuedWeightRMB) {
                     plugMessenger.info("请输入普通用户人名币续重费用!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.firstWeightRMBVip) {
                     plugMessenger.info("请输入VIP用户人名币首重费用!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.continuedWeightRMBVip) {
                     plugMessenger.info("请输入VIP用户人名币续重费用!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.insuranceFeeRate) {
                     plugMessenger.info("请输入自购报费税率!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.RMBExchangeRate) {
                     plugMessenger.info("请输入人名币汇率!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.estimatedTime1) {
                     plugMessenger.info("请输入递送时间!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.estimatedTime2) {
                     plugMessenger.info("请输入递送时间!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.maxWeight) {
                     plugMessenger.info("请输入最大重量(磅)!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.split_roundup) {
                     plugMessenger.info("请输入分箱进位>=!");
-                    return;
+                    return false;
                 }
                 if (!$scope.form.merge_roundup) {
                     plugMessenger.info("请输入最大重量(磅)!");
-                    return;
+                    return false;
                 }
-                
+                $scope.form.shipFeeList = [];
                 // 普通客户人民币
                 var item1 = {
                     isVip: 0,
@@ -166,7 +181,7 @@ angular.module('culAdminApp')
                     currency: 'USD'
                 }
                 $scope.form.shipFeeList.push(item3)
-                $scope.form.warehouseList = $scope.warehouseList
+                return true;
             }
 
             //记录选项状态更改
@@ -178,43 +193,55 @@ angular.module('culAdminApp')
                 else{
                     currentFunc.close = true;
                 }
+                console.log(currentFunc);
             };
 
             // 仓库启用列表
-            var getFormWarehouseList = function(){
+            $scope.getFormWarehouseList = function(){
+                $scope.form.warehouseList = [];
                 if (!$scope.warehouseList)
-                    return;
+                    return false;
                 for (var i = 0; i < $scope.warehouseList.length; i++){
-                    if ($scope.warehouseList[i].status == 1)
+                    if ($scope.warehouseList[i].status === "1") {
                         $scope.form.warehouseList.push($scope.warehouseList[i])
-                }           
+                    }      
+                } 
+                console.log($scope.form.warehouseList)          
             }
 
             // 渠道启用列表
-            var getFormChannelList = function(){
+            $scope.getFormChannelList = function(){
+                $scope.form.channelList = [];
                 if (!$scope.channelList)
-                    return;
+                    return false;
                 for (var i = 0; i < $scope.channelList.length; i++){
-                    if ($scope.channelList[i].status == 1)
+                    if ($scope.channelList[i].status === "1")
                         $scope.form.channelList.push($scope.channelList[i])
                 }              
             }
 
             // 商品主类别启用列表
-            var getFormCarrierList = function(){
-                if (!$scope.carrierList)
-                    return;
-                for (var i = 0; i < $scope.carrierList.length; i++){
-                    if ($scope.carrierList[i].status == 1){
-                        $scope.form.carrierList.push($scope.carrierList[i]);
-                        $scope.form.carrierList[i].children = [];
-                        for (var i = 0; j < $scope.carrierList[i].children.length; i++){
-                            if ($scope.carrierList[i].children[j].status == 1){
-                                $scope.form.carrierList[i].children.push($scope.carrierList[i].children[j]);
+            $scope.getFormCarrierList = function(){
+                $scope.form.carrierList = [];
+                if (!$scope.goodCategoryList)
+                    return false;
+                for (var i = 0; i < $scope.goodCategoryList.length; i++){
+                    console.log($scope.goodCategoryList)  
+                    if ($scope.goodCategoryList[i].status === "1"){
+                        $scope.form.carrierList.push($scope.goodCategoryList[i]);
+                        var fLength = $scope.form.carrierList.length
+                        $scope.form.carrierList[fLength - 1].children = [];
+                        for (var j = 0; j < $scope.goodCategoryList[i].children.length; j++){
+                            if ($scope.goodCategoryList[i].children[j].status === "1"){
+                                $scope.form.carrierList[fLength - 1].children.push($scope.goodCategoryList[i].children[j]);
                             }
                         }
+                        console.log("$scope.form.carrierList")    
+                        console.log($scope.form.carrierList)
+                        console.log($scope.goodCategoryList)    
                     }                     
-                }              
+                }
+                console.log($scope.form.carrierList)              
             }
 
             $('#tip_RMB').popover({
