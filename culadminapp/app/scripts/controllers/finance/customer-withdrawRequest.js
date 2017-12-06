d79a22273cf4dc868001716a595d832a72e789f6
Tim Z Liu <tim.z.liu@pwc.com>
2017-12-06 23:33:44 +0800

    new func

--- culadminapp/app/scripts/controllers/finance/customer-withdrawRequest.js
+++ culadminapp/app/scripts/controllers/finance/customer-withdrawRequest.js
@@ -6,10 +6,10 @@
  * @description
  * # OrderListCtrl 
  * Controller of the culAdminApp
- */ 
+ */
 angular.module('culAdminApp')
-    .controller('CustomerWithdrawCtl', ["$timeout", "$window", "$scope", "$rootScope", "$location", "$filter", "plugMessenger","storage","$compile","customerService",
-        function ($timeout, $window, $scope, $rootScope, $location, $filter, plugMessenger,storage, $compile, customerService) {
+    .controller('CustomerWithdrawCtl', ["$timeout", "$window", "$scope", "$rootScope", "$location", "$filter", "plugMessenger", "storage", "$compile", "customerService",
+        function ($timeout, $window, $scope, $rootScope, $location, $filter, plugMessenger, storage, $compile, customerService) {
             this.awesomeThings = [
                 'HTML5 Boilerplate',
                 'AngularJS',
@@ -40,7 +40,7 @@ angular.module('culAdminApp')
 
                 if (!!$scope.searchBar.status) {
                     _options["status"] = $scope.searchBar.status;
-                } 
+                }
 
                 if (!!$scope.searchBar.keywords) {
                     _options[$scope.searchBar.keywordType] = $scope.searchBar.keywords;
@@ -52,7 +52,7 @@ angular.module('culAdminApp')
                 storage.session.setObject("searchBar", $scope.searchBar);
                 var _options = _filterOptions();
                 customerService.getWithdrawRequestList(angular.copy(_options), function (result) {
-                    if(!result || !result.data || result.data.length < 1) return;
+                    if (!result || !result.data || result.data.length < 1) return;
 
                     // $.each(result.data.data,function(){
                     //     if(this.status == 'P')
@@ -89,23 +89,23 @@ angular.module('culAdminApp')
                 }
             }
 
-            $scope.btnAccept = function(item){
-                if(!item) return;
+            $scope.btnAccept = function (item) {
+                if (!item) return;
 
                 plugMessenger.confirm("确定要接受提款申请吗?", function (isOK) {
                     if (!!isOK) {
 
                         var data = {
-                            transactionNumber:item.transactionNumber,
+                            transactionNumber: item.transactionNumber,
                             customerNumber: item.customerNumber,
-                            refundAmount:item.requestAmount,
+                            refundAmount: item.requestAmount,
                             status: 'A'
                             // alipay_refund_batch_num:item.alipay_refund_batch_num,
                             // alipay_refund_detail_data:item.alipay_refund_detail_data
                         };
 
                         customerService.confirmWithdrawRequest(data, function (result) {
-                            if (result.code == '000') {                                
+                            if (result.code == '000') {
                                 plugMessenger.info("操作成功");
                                 $scope.getData();
                             } else {
@@ -121,17 +121,22 @@ angular.module('culAdminApp')
                 plugMessenger.template($compile($("#decline_note_form").html())($scope));
             }
 
-            $scope.btnDecline = function(item){
-                if(!item) return;
+            $scope.btnDecline = function (item) {
+                if (!item) return;
 
                 plugMessenger.confirm("确定要拒绝提款申请吗?", function (isOK) {
                     if (!!isOK) {
                         $("#confirm-modal").modal("hide");
 
                         var data = {
-                            transactionNumber:item.transactionNumber,
+                            transactionNumber: item.transactionNumber,
                             status: 'D',
-                            note: $scope.declineNote
+                            note: $scope.declineNote,
+                            customerNumber: item.customerNumber,
+                            requestAmount: item.requestAmount,
+                            memo: "管理员拒绝提现申请",
+                            operationType: "4",
+                            payment: -(Number(item.requestAmount) + 20)
                             // alipay_refund_batch_num:item.alipay_refund_batch_num,
                             // alipay_refund_detail_data:item.alipay_refund_detail_data
                         };
@@ -157,13 +162,13 @@ angular.module('culAdminApp')
                 plugMessenger.template($compile($("#confirm_refund_form").html())($scope));
             }
 
-            function isInt(n){
-                if(n !== n) return false;
+            function isInt(n) {
+                if (n !== n) return false;
                 return parseInt(n) === n || n % 1 !== 0;
             }
 
-            $scope.btnConfirm = function(item){
-                if(!item) return;
+            $scope.btnConfirm = function (item) {
+                if (!item) return;
 
                 if (!$scope.confirmRefundAmount) {
                     return plugMessenger.info("实际退款金额不能为空");
@@ -175,10 +180,10 @@ angular.module('culAdminApp')
                     return plugMessenger.info("实际退款金额格式不正确,请输入数字金额");
                 }
 
-                if (parsedRefundAmount + 20 > item.accountBalance) {
-                    alertify.alert('提醒', '您账户余额至少需要￥' + parseFloat(parsedRefundAmount + 20).toFixed(2) + ': 申请退款金额￥'+ parsedRefundAmount.toFixed(2) + ',单次退款手续费￥20.', 'warning');
-                    return false;
-                }
+                // if (parsedRefundAmount + 20 > item.accountBalance) {
+                //     plugMessenger.info('提醒', '您账户余额至少需要￥' + parseFloat(parsedRefundAmount + 20).toFixed(2) + ': 申请退款金额￥'+ parsedRefundAmount.toFixed(2) + ',单次退款手续费￥20.', 'warning');
+                //     return false;
+                // }
 
                 if (parsedRefundAmount.toFixed(2) != item.requestAmount && !$scope.confirmNote) {
                     return plugMessenger.info("实际退款不等于申请金额,必须输入备注信息");
@@ -189,8 +194,8 @@ angular.module('culAdminApp')
                         $("#confirm-modal").modal("hide");
 
                         var data = {
-                            transactionNumber:item.transactionNumber,
-                            refundAmount:parsedRefundAmount.toFixed(2),
+                            transactionNumber: item.transactionNumber,
+                            refundAmount: parsedRefundAmount.toFixed(2),
                             status: 'R',
                             customerNumber: item.customerNumber,
                             note: $scope.confirmNote
