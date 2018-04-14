4031de4a410ecab271a4c195c712e82435ec3aeb
Lisa <Lisa@LisaMacBook-Pro.local>
2018-04-14 11:21:22 +0800

    屏蔽清点拍照

--- culwebapp/app/scripts/services/orderService.js
+++ culwebapp/app/scripts/services/orderService.js
@@ -185,7 +185,7 @@ angular.module('culwebApp')
                     //加套外箱 0,1
                     pack_addCarton: orderData.pack_addCarton,
                     //清点拍照 0,1
-                    pack_checkCount: orderData.pack_checkCount,
+                    // pack_checkCount: orderData.pack_checkCount,
                     //购买保险标识 0,1
                     insuranceMark: (orderData.insuranceMark ? 1 : 0),
                     //使用积分标识 0,1
