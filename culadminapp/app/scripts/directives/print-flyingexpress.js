8fe6335725ffce8afb6691361000c6fdb35e9615
Lisa5 <1348894516@qq.com>
2018-04-15 22:42:30 +0800

    bug fix

--- culadminapp/app/scripts/directives/print-flyingexpress.js
+++ culadminapp/app/scripts/directives/print-flyingexpress.js
@@ -34,7 +34,6 @@ angular.module('culAdminApp')
                       "orderNumber": $scope.orderNumbers
                   }, function (result) {
                       var _orderItems = {};
-                      console.log(result);
                       $.each(result.data, function (index, row) {
                           if (!_orderItems[row.orderNumber]) {
                               _orderItems[row.orderNumber] = row.items;
@@ -81,9 +80,7 @@ angular.module('culAdminApp')
                                   });
                               }
                           }
-
                       });
-                    
                       $scope.dataList = result.data;
                       _render();
                   });
